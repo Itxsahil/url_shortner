806352fcef2d64eb8fd5088e0469a8ce9ab769f8
itxsahil <sahilkh9087@gmail.com>
2024-05-19 19:44:40 +0530

    all bug fixed

--- index.js
+++ index.js
@@ -1,58 +1,71 @@
-require('dotenv').config()
-const express = require('express')
+require('dotenv').config();
+const express = require('express');
 const path = require('path');
-const app = express()
+const mongoose = require('mongoose');
 
+const app = express();
+const PORT = process.env.PORT || 3000; // Define PORT here
 
-
-const mongoose = require('mongoose')
-
-mongoose.connect(`${process.env.MONGO_DB_URI}newdb`)
+mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Could not connect to MongoDB', err));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("static"));
 
 const urlSchema = new mongoose.Schema({
-    mainurl : String,
-    shorturl : String
-})
+    mainurl: String,
+    shorturl: String
+});
 
-const newuri = mongoose.model("newuri",urlSchema)
-
-
+const newuri = mongoose.model("newuri", urlSchema);
 
 function generateString(length) {
-    const characters ='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
     let result = '';
     const charactersLength = characters.length;
-    for ( let i = 0; i < length; i++ ) {
+    for (let i = 0; i < length; i++) {
         result += characters.charAt(Math.floor(Math.random() * charactersLength));
     }
-
     return result;
 }
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'))
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+app.post('/create', async (req, res) => {
+    try {
+        const url = req.body.url;
+        const shortUrlId = generateString(5);
+        const ShortUrl = new newuri({
+            mainurl: url,
+            shorturl: `http://localhost:${PORT}/${shortUrlId}`
+        });
+        const savedShortUrl = await ShortUrl.save();
+        console.log(savedShortUrl);
+        res.json(savedShortUrl.shorturl);
+    } catch (error) {
+        console.error('Error creating short URL:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
-app.post('/create',async(req, res)=>{
-    const url = req.body.url
-    const ShortUrl =  new newuri({
-        mainurl : url,
-        shorturl : "http://localhost:"+ PORT + "/" + generateString(5),        
-    })
-    const shortUrl = await ShortUrl.save()
-    console.log(shortUrl)
-    res.json(shortUrl.shorturl)
-})
+app.get('/:id', async (req, res) => {
+    try {
+        const id = req.params.id;
+        const shortUrl = `http://localhost:${PORT}/${id}`;
+        const url = await newuri.findOne({ shorturl: shortUrl });
+        if (url) {
+            res.redirect(url.mainurl);
+        } else {
+            res.status(404).json({ message: 'URL not found' });
+        }
+    } catch (error) {
+        console.error('Error finding URL:', error);
+        res.status(500).json({ message: 'Internal server error' });
+    }
+});
 
-app.get('/:id',async(req, res )=>{
-    const id = req.params.id
-    const url = await newuri.findOne({shorturl : "http://localhost:"+ process.env.PORT + "/" + id})
-    res.redirect(url.mainurl)
-})
-
-app.listen(process.env.PORT, ()=> console.log(`Server is livr on port ${process.env.PORT} .....`))
+app.listen(PORT, () => console.log(`Server is live on port ${PORT}...`));
